4803189256f08528ed6f73bb75f65acc541deaad
RajeshwariSonawane17 <sonawanerajeshwari414@gmail.com>
2025-04-05 12:39:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example: Optional JS for image hover animations or other interactivity
+$(document).ready(function() {
+    // Example: Submit form alert
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        alert('Your email has been submitted!');
+    });
+});
